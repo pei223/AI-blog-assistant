1411d73a46dbe3e9d7b956d2b675a01edfe45bd8
pei <peidparc@gmail.com>
2023-02-04 11:50:56 +0900

    ロード画面コンポーネント暫定作成

---
+++ src/components/atoms/LoadingScreen.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { CircularProgress } from "@mui/material";
+import styled from "styled-components";
+
+const StyledFullScreen = styled.div`
+  z-index: 9999;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
+
+const StyledProgressWrapper = styled.div`
+  position: relative;
+  top: 45vh;
+`;
+
+type Props = {
+  text: string;
+};
+
+const LoadingScreen: React.FC<Props> = ({ text = "ロード中" }) => {
+  return (
+    <StyledFullScreen>
+      <StyledProgressWrapper>
+        <CircularProgress color="primary" size={50} />
+        <p style={{ margin: "0" }}>{text}</p>
+      </StyledProgressWrapper>
+    </StyledFullScreen>
+  );
+};
+
+export default LoadingScreen;
