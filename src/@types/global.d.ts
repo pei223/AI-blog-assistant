7cd48f46d0238fd69fb3c701a4acc81678f73e3f
pei <peidparc@gmail.com>
2023-02-04 11:28:06 +0900

    型定義ファイル追加

---
+++ src/@types/global.d.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="node">
+
+declare namespace NodeJS {
+  interface ImportMetaEnv {
+    MODE: "development" | "production"
+  }
+}
+
+interface Window {
+  mainProcess: MainProcess;
+}
+declare let window: Window
+
+interface MainProcess {
+  getAPIKey: () => Promise<string | error>;
+  setAPIKey: (keyVal: string) => Promise<void>;
+  generateText: (text: string, option: GenerateOption) => Promise<string>;
+}
