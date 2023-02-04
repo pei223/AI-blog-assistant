4e64d4f39c7b12309977c12b311cd37c92b07132
pei <peidparc@gmail.com>
2023-02-04 12:18:01 +0900

    初期設定画面暫定作成設定画面暫定作成

---
+++ src/pages/InitSetting.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { Button, Container, TextField, Typography } from "@mui/material";
+import { useNavigate } from "react-router";
+import { getAPIKey, setAPIKey } from "../main-module";
+import { useErrorHandler } from "react-error-boundary";
+
+const InitSetting = () => {
+  const [apiKeyVal, setApiKeyVal] = useState("");
+  const navigate = useNavigate();
+  const errHandler = useErrorHandler();
+
+  useEffect(() => {
+    getAPIKey().then((val: string | Error) => {
+      // TODO 
+      if (val instanceof Error ) {
+        errHandler(val);
+        return
+      }
+      setApiKeyVal(val);
+    }).catch((e) => {
+      errHandler(e);
+    });
+  }, []);
+
+  const onSubmit = () => {
+    setAPIKey(apiKeyVal).then(() => {
+      navigate("/generate");
+    }).catch((e) => {
+      errHandler(e);
+    });
+  };
+
+  return (
+    <Container>
+      <Typography>Enter OpenAI API key</Typography>
+      <TextField
+        value={apiKeyVal}
+        onChange={(e) => {
+          setApiKeyVal(e.target.value);
+        }}
+      />
+      <Button onClick={onSubmit}>Start</Button>
+    </Container>
+  );
+};
+export default InitSetting;
