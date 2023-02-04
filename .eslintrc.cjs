5800c797564d2673cfd3773683e406e0815266d4
pei <peidparc@gmail.com>
2023-02-04 12:15:30 +0900

    ESLint設定暫定作成

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
     'react'
   ],
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off"
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off"
   }
 }
