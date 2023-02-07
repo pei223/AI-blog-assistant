bcc75471f854d22fa6c3d05ef1a06e06e42dc21f
pei <peidparc@gmail.com>
2023-02-07 22:06:44 +0900

    AI設定から読み込んだ値で生成するようにする

--- src/pages/Generate.tsx
+++ src/pages/Generate.tsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Grid, TextField, Typography } from '@mui/material'
 import { useErrorHandler } from 'react-error-boundary'
 import Layout from '../Layout'
 import { generateFromTemplate } from '../template-mod'
-import { ContentTemplate, SummaryTemplate } from '../openai/template'
 import styled from 'styled-components'
-import { type GenerateOption } from '../openai/types'
+import {
+  type AiSettingDict,
+  CONTENT_SETTING_KEY,
+  INITIAL_CONTENT_AI_SETTING,
+  INITIAL_SUMMARY_AI_SETTING,
+  SUMMARY_SETTING_KEY
+} from '../openai/types'
 import LoadingScreen from '../components/atoms/LoadingScreen'
 import { generateText } from '../main-module'
-
-const defaultContentGenerateOption: GenerateOption = {
-  model: 'text-davinci-003',
-  max_tokens: 2048
-}
 
 const StyledButtonWrapper = styled.div`
   text-align: center;
@@ -25,28 +25,45 @@ const Generate = () => {
   const [title, setTitle] = useState('')
   const [summary, setSummary] = useState('')
   const [content, setContent] = useState('')
+  const [aiSettingDict, setAiSettingDict] = useState<AiSettingDict>({})
 
   const errHandler = useErrorHandler()
+
+  useEffect(() => {
+    window.mainProcess
+      .getAiSettingDict()
+      .then((dict) => {
+        setAiSettingDict(dict)
+      })
+      .catch((e) => {
+        errHandler(e)
+      })
+  })
 
   const generateContentFromTitle = () => {
     const inner = async () => {
       setLoading(true)
+      const summaryAiSetting =
+        aiSettingDict[SUMMARY_SETTING_KEY] ?? INITIAL_SUMMARY_AI_SETTING
+      const contentAiSetting =
+        aiSettingDict[CONTENT_SETTING_KEY] ?? INITIAL_CONTENT_AI_SETTING
       try {
-        const summaryGenText = generateFromTemplate(SummaryTemplate, {
+        const { template: summaryTemplate, ...summaryGenOption } =
+          summaryAiSetting
+        const { template: contentTemplate, ...contentGenOption } =
+          contentAiSetting
+
+        const summaryGenText = generateFromTemplate(summaryTemplate, {
           title
         })
-
-        const _summary = await generateText(summaryGenText)
+        const _summary = await generateText(summaryGenText, summaryGenOption)
         setSummary(_summary)
-        const contentGenText = generateFromTemplate(ContentTemplate, {
+
+        const contentGenText = generateFromTemplate(contentTemplate, {
           title,
           summary: _summary
         })
-        // @ts-check
-        const _content = await generateText(
-          contentGenText,
-          defaultContentGenerateOption
-        )
+        const _content = await generateText(contentGenText, contentGenOption)
         setContent(_content)
       } catch (e) {
         errHandler(e)
@@ -63,15 +80,14 @@ const Generate = () => {
     const inner = async () => {
       setLoading(true)
       try {
-        const contentGenText = generateFromTemplate(ContentTemplate, {
+        const contentAiSetting =
+          aiSettingDict[CONTENT_SETTING_KEY] ?? INITIAL_CONTENT_AI_SETTING
+        const { template, ...contentGenOption } = contentAiSetting
+        const contentGenText = generateFromTemplate(template, {
           title,
           summary
         })
-        // @ts-check
-        const _content = await generateText(
-          contentGenText,
-          defaultContentGenerateOption
-        )
+        const _content = await generateText(contentGenText, contentGenOption)
         setContent(_content)
       } catch (e) {
         errHandler(e)
