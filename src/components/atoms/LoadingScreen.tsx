6b9dd3974804d5a604e04a6c73cb34b2fb0e30f6
pei <peidparc@gmail.com>
2023-02-09 23:21:08 +0900

    ロード画面コンポーネント修正

--- src/components/atoms/LoadingScreen.tsx
+++ src/components/atoms/LoadingScreen.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { CircularProgress } from "@mui/material";
-import styled from "styled-components";
+import React from 'react'
+import { CircularProgress, Typography } from '@mui/material'
+import styled from 'styled-components'
 
 const StyledFullScreen = styled.div`
   z-index: 9999;
@@ -10,27 +10,31 @@ const StyledFullScreen = styled.div`
   width: 100vw;
   height: 100vh;
   text-align: center;
-  background-color: rgba(0, 0, 0, 0.3);
-`;
+  background-color: rgba(0, 0, 0, 0.4);
+`
 
 const StyledProgressWrapper = styled.div`
   position: relative;
   top: 45vh;
-`;
+`
 
 type Props = {
-  text?: string;
-};
+  text?: string
+}
 
 const LoadingScreen: React.FC<Props> = ({ text }) => {
   return (
     <StyledFullScreen>
       <StyledProgressWrapper>
-        <CircularProgress color="primary" size={50} />
-        {text != null && <p style={{ margin: "0" }}>{text}</p>}
+        <CircularProgress color="primary" size={70} />
+        {text != null && (
+          <Typography variant="h6" marginTop={2}>
+            {text}
+          </Typography>
+        )}
       </StyledProgressWrapper>
     </StyledFullScreen>
-  );
-};
+  )
+}
 
-export default LoadingScreen;
+export default LoadingScreen
