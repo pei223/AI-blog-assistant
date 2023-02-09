79c92f3472346f690e186332a108312951050db7
pei <peidparc@gmail.com>
2023-02-09 22:17:21 +0900

    テンプレート修正

--- src/openai/template.ts
+++ src/openai/template.ts
@@ -1,12 +1,15 @@
-export const SummaryTemplate = `
-下記のタイトルのブログ記事の詳細な目次を書いてください。
+export const SummaryTemplate = `下記のタイトルのブログ記事の目次を箇条書きで詳細に書いてください。
+
 タイトル：<title>
+
 `
 
-export const ContentTemplate = `
-以下のブログ記事の目次とタイトルを参考に、ブログ記事を作成してください。
+export const ContentTemplate = `以下のブログ記事の目次とタイトルを参考に、ブログ記事を作成してください。
 ストーリーのある文章にしてください。
 一つ一つを深く掘り下げて、なぜそうなるのかを説明してください。
+
 タイトル：<title>
+
 目次：<summary>
+
 `
