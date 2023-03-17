20117aa81084052c9adeda736d5bcd60653b740a
pei <peidparc@gmail.com>
2023-03-17 23:17:38 +0900

    長文ブログ生成画面実装

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { createTheme, ThemeProvider } from '@mui/material'
 import Generate from './pages/generate/Generate'
 import Settings from './pages/Settings'
 import { indigo, teal } from '@mui/material/colors'
+import GenerateLongText from './pages/generate/GenerateLongText'
 
 const theme = createTheme({
   spacing: 8,
@@ -29,6 +30,7 @@ const AppRouter = () => {
         <Route path="/" element={<Init />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/generate" element={<Generate />} />
+        <Route path="/generate-long-text" element={<GenerateLongText />} />
       </Routes>
     </HashRouter>
   )
